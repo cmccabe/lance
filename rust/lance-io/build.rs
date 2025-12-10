64873cdf4938e8adab1f2cceff51daa2d65255f9
Colin P. McCabe <cmccabe@apache.org>
2025-12-09 22:03:07 -0800

    more proto, redux

--- rust/lance-io/build.rs
+++ rust/lance-io/build.rs
@@ -3,17 +3,16 @@ use std::io::Result;
 fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=protos");
 
-    #[cfg(feature = "protoc")]
     // Use vendored protobuf compiler if requested.
     std::env::set_var("PROTOC", protobuf_src::protoc());
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_client(true)
         .build_server(true)
-        protoc_arg("--experimental_allow_proto3_optional")
-        .compile(
-            &["./protos/avalon.proto"],
-            &["./protos"],
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(
+            &["../../protos/avalon.proto"],
+            &["../../protos"],
         )?;
 
     Ok(())
