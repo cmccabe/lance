8a2f68a9e58988f4976f4acd2ae844a99cd18d21
Colin P. McCabe <cmccabe@apache.org>
2025-12-10 13:27:54 -0800

    object_store/providers.rs: add 'use std::io::Write'

--- rust/lance-io/src/object_store/providers.rs
+++ rust/lance-io/src/object_store/providers.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright The Lance Authors
 
+use std::io::Write;
+
 use std::{
     collections::HashMap,
     sync::{Arc, RwLock, Weak},
